0dfd15f0b79d8f07a2755d4153b2ba45dedfdad7
zou_shengli <zou_shengli@cdv.com>
2018-01-12 15:17:02 +0800

    require.js暂不会用,屏蔽,防止报错

--- src/main/resources/static/resources/session/view/activiti.js
+++ src/main/resources/static/resources/session/view/activiti.js
@@ -22,3 +22,29 @@ App.controller('activitiController', ['$scope', '$http', '$timeout', '$uibModal'
 
 
 }]);
+
+/*define(function(require) {
+	
+	var app = require('app.js');
+	
+	app.controller('activitiController', function(){
+		
+		$scope.condition = {
+			moreShow: false, //是否显示更多
+			funcMore: function() {
+				this.moreShow = !this.moreShow;
+			},
+	    };
+		$scope.activitiEditor = {
+	    	createTables: function() {
+	    		 $http({
+	                 url:'/ns/task',
+	                 method:'POST',
+	             }).success(function (resp) {
+	                 console.log(resp);
+	             });
+	    	}
+	    };
+	})
+	
+})*/
